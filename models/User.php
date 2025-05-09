923857b1a612ef7236040fe3d2501161e59e7d64
K-Dog <103394438+kayle-henry@users.noreply.github.com>
2025-05-08 20:14:17 -0500

    Update User.php

--- models/User.php
+++ models/User.php
@@ -1,74 +1,48 @@
 <?php
-    class User{
-        private $userID;
-        private $username;
-        private $lastname;
-        private $firstname;
-        private $email;
-        private $passwd;
-        private $role;
+class User {
+    private int $id;
+    private string $username;
+    private string $password;
+    private string $role;
 
-        public function load($row){
-            $this->setUserID($row['userID']);
-            $this->setUsername($row['username']);
-            $this->setLastName($row['lastname']);
-            $this->setFirstName($row['firstname']);
-            $this->setEmail($row["email"]);
-            $this->setPasswd($row["passwd"]);
-            $this->setRole($row['role']);
-        }
-
-        public function setUserID($contactID){
-            $this->contactID=$contactID;
-        }
-
-        public function getUserID(){
-            return $this->contactID;
-        }
-
-        public function setUsername($username){
-            $this->username=$username;
-        }
-
-        public function getUsername(){
-            return $this->username;
-        }
-        public function setFirstName($firstname){
-            $this->firstname=$firstname;
-        }
-
-        public function getFirstName(){
-            return $this->firstname;
-        }
-        public function setLastName($lastname){
-            $this->lastname=$lastname;
-        }
-
-        public function getLastName(){
-            return $this->lastname;
-        }
-
-        public function setEmail($email){
-            $this->email=$email;
-        }
-
-        public function getEmail(){
-            return $this->email;
-        }
-
-        public function setPasswd($passwd){
-            $this->passwd=$passwd;
-        }
-
-        public function getPasswd(){
-            return $this->passwd;
-        }
-        public function setRole($passwd){
-            $this->role=$role;
-        }
-
-        public function getRole(){
-            return $this->role;
-        }
+    public function __construct(int $id = 0, string $username = '', string $password = '', string $role = 'unauthenticated') {
+        $this->id = $id;
+        $this->username = $username;
+        $this->password = $password;
+        $this->role = $role;
     }
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function setId(int $id): void {
+        $this->id = $id;
+    }
+
+    public function getUsername(): string {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): void {
+        $this->username = $username;
+    }
+
+    public function getPassword(): string {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void {
+        $this->password = $password;
+    }
+
+    public function getRole(): string {
+        return $this->role;
+    }
+
+    public function setRole(string $role): void {
+        $this->role = $role;
+    }
+}
+
 ?>

507d7c22d8945b2667b439668489d2012c1f6d3d
K-Dog <103394438+kayle-henry@users.noreply.github.com>
2025-05-08 20:28:13 -0500

    Create home.php

---
+++ views/home.php
@@ -0,0 +1,7 @@
+<?php
+// This is the homepage of the application, displaying an overview or welcome message.
+
+echo "<h1>Welcome to the PHP Project</h1>";
+echo "<p>This is the homepage of the application.</p>";
+echo "<p>Explore the topics and articles available.</p>";
+?>

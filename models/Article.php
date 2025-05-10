4dd1f4708c5891ff6c64b317c1c395af980dd0a8
Ruisuuu <111672090+Ruisuuu@users.noreply.github.com>
2025-05-09 23:39:25 -0500

    Update Article.php

--- models/Article.php
+++ models/Article.php
@@ -1,59 +1,33 @@
 <?php
+// models/Article.php
 
 class Article {
-    private int $id;
-    private string $title;
-    private string $content;
-    private int $topicId;
-    private int $authorId;
+    private $artID;
+    private $title;
+    private $image;
+    private $content;
+    private $authorID;
+    private $catID;
+    private $lastModified;
 
-    public function __construct(int $id = 0, string $title = '', string $content = null, int $topicId = 0, int $authorId = 0) {
-        $this->id = $id;
-        $this->title = $title;
-        $this->content = $content;
-        $this->topicId = $topicId;
-        $this->authorId = $authorId;
-    }
+    public function getArtID() { return $this->artID; }
+    public function setArtID($artID) { $this->artID = $artID; }
 
-    public function getId(): int {
-        return $this->id;
-    }
+    public function getTitle() { return $this->title; }
+    public function setTitle($title) { $this->title = $title; }
 
-    public function setId(int $id): void {
-        $this->id = $id;
-    }
+    public function getImage() { return $this->image; }
+    public function setImage($image) { $this->image = $image; }
 
-    public function getTitle(): string {
-        return $this->title;
-    }
+    public function getContent() { return $this->content; }
+    public function setContent($content) { $this->content = $content; }
 
-    public function setTitle(string $title): void {
-        $this->title = $title;
-    }
+    public function getAuthorID() { return $this->authorID; }
+    public function setAuthorID($authorID) { $this->authorID = $authorID; }
 
-    public function getContent(): string {
-        return $this->content;
-    }
+    public function getCatID() { return $this->catID; }
+    public function setCatID($catID) { $this->catID = $catID; }
 
-    public function setContent(string $content): void {
-        $this->content = $content;
-    }
-
-    public function getTopicId(): int {
-        return $this->topicId;
-    }
-
-    public function setTopicId(int $topicId): void {
-        $this->topicId = $topicId;
-    }
-
-    public function getAuthorId(): int {
-        return $this->authorId;
-    }
-
-    public function setAuthorId(int $authorId): void {
-        $this->authorId = $authorId;
-    }
+    public function getLastModified() { return $this->lastModified; }
+    public function setLastModified($lastModified) { $this->lastModified = $lastModified; }
 }
-
-?>

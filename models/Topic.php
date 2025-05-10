52e1c1c22350b6624326e19d08bcc3bacf846778
Ruisuuu <111672090+Ruisuuu@users.noreply.github.com>
2025-05-09 23:40:51 -0500

    Update Topic.php

--- models/Topic.php
+++ models/Topic.php
@@ -1,49 +1,16 @@
 <?php
+// models/Topic.php
 
 class Topic {
-    private int $id;
-    private string $name;
-    private string $description;
-    private int $createdBy;
+    private $topID;
+    private $name;
+    private $description;
 
-    public function __construct(int $id = 0, string $name = '', string $description = null, int $createdBy = 0) {
-        $this->id = $id;
-        $this->name = $name;
-        $this->description = $description;
-        $this->createdBy = $createdBy;
-    }
+    public function getTopID() { return $this->topID; }
+    public function setTopID($topID) { $this->topID = $topID; }
 
-    public function getId(): int {
-        return $this->id;
-    }
+    public function getName() { return $this->name; }
+    public function setName($name) { $this->name = $name; }
 
-    public function setId(int $id): void {
-        $this->id = $id;
-    }
-
-    public function getName(): string {
-        return $this->name;
-    }
-
-    public function setName(string $name): void {
-        $this->name = $name;
-    }
-
-    public function getDescription(): string {
-        return $this->description;
-    }
-
-    public function setDescription(string $description): void {
-        $this->description = $description;
-    }
-
-    public function getCreatedBy(): int {
-        return $this->createdBy;
-    }
-
-    public function setCreatedBy(int $createdBy): void {
-        $this->createdBy = $createdBy;
-    }
-}
-
-?>
+    public function getDescription() { return $this->description; }
+    public function setDescription($description) { $this->description = $description; }
